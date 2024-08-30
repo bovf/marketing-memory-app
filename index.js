20bc919aa73774e62069c15491ad4fb3f86ef5a2
bovfbovf <dobry989@gmail.com>
2024-08-30 12:28:47 +0300

    revert

--- index.js
+++
@@ -1,9 +0,0 @@
-window.onload = function() {
-    setTimeout(function() {
-        document.getElementById('popupMessage').style.display = 'block';
-    }, 2000);
-}
-
-function closePopup() {
-    document.getElementById('popupMessage').style.display = 'none';
-}
